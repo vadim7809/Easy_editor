236f5136ec27bf2766c448132a79df1976a68791
Vislyy <mandyuk.vadim@gmail.com>
2025-03-30 16:29:58 +0300

    py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QPushButton, QVBoxLayout, QHBoxLayout
+import os
+
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtWidgets import *
+
+from PIL import Image, ImageEnhance, ImageFilter
+
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
 
 app = QApplication([])
 
@@ -8,7 +27,7 @@ window = QWidget()
 window.resize(600, 400)
 
 
-main_line = QHBoxLayout(window)
+main_line = QHBoxLayout()
 
 
 left_line = QVBoxLayout()
@@ -22,12 +41,8 @@ file_list = QListWidget()
 file_list.addItems(["bright.png", "field.png", "trees.png", "villiage.png"])
 left_line.addWidget(file_list)
 
-
 v1_line = QVBoxLayout()
-
-
 img_lbl = QLabel()
-
 v1_line.addWidget(img_lbl)
 
 btn_layout = QHBoxLayout()
@@ -38,8 +53,6 @@ right_btn = QPushButton("Вправо")
 mirror_btn = QPushButton("Дзеркало")
 sharp_btn = QPushButton("Різкість")
 bw_btn = QPushButton("Ч/Б")
-
-
 
 
 btn_layout.addWidget(left_btn)
@@ -55,10 +68,65 @@ v1_line.addLayout(btn_layout)
 main_line.addLayout(left_line)
 main_line.addLayout(v1_line)
 
+class ImageProcessor:
+    def __init__(self):
+        self.folder = ""
+        self.filename = ""
+        self.image = ""
+    def load(self):
+        img_path = os.path.join(self.folder, self.filename)
+        self.image = Image.open(img_path)
+    def show(self):
+        pix = pil2pixmap(self.image)
+        pix= pix.scaledToWidth(500)
+        img_lbl.setPixmap(pix)
 
+    def rotate_left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.show()
+
+    def rotate_right(self):
+        self.image = self.image.transpose(Image.ROTATE_270)
+        self.show()
+
+    def mirror(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.show()
+
+    def sharp(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.show()
+
+    def black_or_white(self):
+        self.image = self.image.convert("L")
+        self.show()
+
+ip = ImageProcessor()
+ip.filename = "img.jpg"
+ip.load()
+ip.show()
+def open_folder():
+    ip.folder = QFileDialog.getExistingDirectory()
+    files = os.listdir(ip.folder)
+    file_list.clear()
+    for file in files:
+        if file.endswith(".jpg"):
+            file_list.addItem(file)
+
+
+def show_chosen_image():
+    ip.filename= img_lbl.currentItem().text()
+    ip.load()
+    ip.show()
+
+bw_btn.clicked.connect(black_or_white)
+sharp_btn.clicked.connect(ip.sharp)
+mirror_btn.clicked.connect(ip.mirror)
+right_btn.clicked.connect(ip.rotate_right)
+left_btn.clicked.connect(ip.rotate_left)
+file_list.currentRowChanged.connect(show_chosen_image)
+folders_btn.clicked.connect(open_folder)
 window.setLayout(main_line)
-
-
 window.show()
 app.exec()
 
